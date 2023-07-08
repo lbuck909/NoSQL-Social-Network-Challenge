5c071d067f69c6fae5c2c55d56b06b891423957d
LaiSha Buck <laishabuck@gmail.com>
2023-07-08 13:08:22 -0400

    virtual friendCount added and module.exports

--- models/user.js
+++ models/user.js
@@ -7,17 +7,40 @@ const userSchema = newSchema(
       unique: true,
       trim: true,
     },
-
+//create a regex email validation
     email:{
       type: String,
       required: true,
       unique: true,
-      validate:{
-        validator: function(v) {
-          return /^([\w\-\.]+)@([\w\-\.]+)\.([a-zA-Z]{2,5})$/.test(v);
-        }
-      } 
-    }
-  }
+      match: [/^([\w\-\.]+)@([\w\-\.]+)\.([a-zA-Z]{2,3})$/, "Wrong emial entered, please enter a valid email address"]
+      }, 
 
-)
+    friends:[
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+
+      },
+    ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought"
+      },
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id:false
+  });
+// the virtual is called friendCount. shows up within query
+  userSchema.virtual("friendCount").get(function() {
+    return 
+    this.friends.length;
+  });
+// create var using User & userschema. finish with module exports for user here
+const User = model("User", userSchema);
+module.exports = User;
