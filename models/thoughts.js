729ee8bad01c764fab01c32612781599e0b7142d
LaiSha Buck <laishabuck@gmail.com>
2023-07-08 15:11:51 -0400

    thoughts js updated. module exports corrected

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,3 +1,67 @@
 const { Schema, model, Types  } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+//thoughtsText, username, reactions, and createdAt
+const thoughtsSchema = new Schema(
+  {
+    thoughtsText: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    reactions: [reactionsSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const reactionsSchema = new Schema(
+  {
+    reactionsId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactionsData: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+
+
+//reactionsCount and virtuals
+thoughtsSchema.virtual('reactionCount').get(function () {
+return this.reactions.length;
+});
+const Thought = model('Thought, thoughtSchema');
+module.exports = Thought;
