083d48aa006c14a35371ca5fcea6f74f8f4863c5
LaiSha Buck <laishabuck@gmail.com>
2023-07-07 20:16:41 -0400

    connection created

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:27017/socialDB',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection
--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const app = express();
 
 app.use(express.urlencoded({extended: true }));
 app.use(routes);
+app.use(express.json());
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
